c8e3bcbcd94d6ddf807354a4df750fab312729e8
ANDREEA TOMA <andre17ea@gmail.com>
2025-09-15 02:19:50 +0100

    Final version at this stage

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Week02-Assignment
+
+##Gallery
+
+Althought most of the points were achieved, the result is not what was expected.
+
+## Rework
+
+A lot of changes and rewrite the code countless times:
+
+- Creating a layout where the **large image fills the entire screen** while keeping the **thumbnail bar on top**.
+- Troubleshooting JavaScript errors related to `undefined` images and bad event handling.
+- Generating properly scaled images (e.g., setting width to 400px and maintaining proportions) - gave up in the end.
+
+## All the resources used up
+
+I encountered more problems than I expected. It took longer than expected due to conflicting layouts goals.
+This project helped me understand the importance of planning layout structure early and the power of responsive design.
--- app.js
+++ app.js
@@ -9,28 +9,103 @@ const images = [
     alt: "A library full of various books",
   },
   { src: "./images/books.jpg", alt: "Collection of books" },
+  {
+    src: "./images/scoici.jpg",
+    alt: "Beautiful shells",
+  },
+  {
+    src: "./images/phone.jpg",
+    alt: "Phone shooting",
+  },
+
+  {
+    src: "./images/map.jpg",
+    alt: "Cameras on world map",
+  },
+
+  {
+    src: "./images/ladybug.jpg",
+    alt: "A ladybug on a leaf",
+  },
+  {
+    src: "./images/egipt.jpg",
+    alt: "Representantion of Ancient Egypt",
+  },
+  {
+    src: "./images/funnyfish.jpg",
+    alt: "A funny ocean fish",
+  },
+  { src: "./images/flamingo.jpg", alt: "A oasis with flamingos" },
+  {
+    src: "./images/dance2.jpg",
+    alt: "Couple dancing",
+  },
+  {
+    src: "./images/arrows.jpg",
+    alt: "Colorful arrows",
+  },
+  {
+    src: "./images/archfall.jpg",
+    alt: "Arrow and bow in fall",
+  },
+  {
+    src: "./images/archery.jpg",
+    alt: "Girl practicing",
+  },
 ];
 
-function createThumbnails() {
-  images.forEach(function (image) {
-    let imageElement = document.createElement("img");
-    console.log(`current image is`, image);
-    imageElement.src = image.src;
+let currentIndex = 0;
 
-    imageElement.addEventListener("click", function () {
-      console.log(image);
-      createBigImage(image);
-    });
-    thumbnails.appendChild(imageElement);
+function showImage(index) {
+  if (index < 0 || index >= images.length) return;
+
+  const imageData = images[index];
+  displayContainer.innerHTML = "";
+
+  const img = document.createElement("img");
+  img.src = imageData.src;
+  img.alt = imageData.alt;
+
+  displayContainer.appendChild(img);
+
+  [...thumbnails.children].forEach((thumb, i) => {
+    thumb.style.borderColor = i === index ? "#f199" : "transparent";
   });
 }
 
-createThumbnails();
+function createThumbnails() {
+  images.forEach((image, index) => {
+    const thumb = document.createElement("img");
+    thumb.src = image.src;
+    thumb.alt = image.alt;
+    thumb.tabIndex = 0;
 
-function createBigImage(imgDetails) {
-  displayContainer.innerHTML = "";
-  const bigImage = document.createElement("img");
-  bigImage.src = imgDetails.src;
-  bigImage.alt = imgDetails.alt;
-  displayContainer.appendChild(big.Image);
+    thumb.addEventListener("click", () => {
+      currentIndex = index;
+      showImage(currentIndex);
+    });
+
+    thumbnails.appendChild(thumb);
+  });
 }
+
+prevButton.addEventListener("click", () => {
+  currentIndex = currentIndex === 0 ? images.length - 1 : currentIndex - 1;
+  showImage(currentIndex);
+});
+
+nextButton.addEventListener("click", () => {
+  currentIndex = currentIndex === images.length - 1 ? 0 : currentIndex + 1;
+  showImage(currentIndex);
+});
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowLeft") {
+    prevButton.click();
+  } else if (e.key === "ArrowRight") {
+    nextButton.click();
+  }
+});
+
+createThumbnails();
+showImage(currentIndex);
